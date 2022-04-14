430d82d58898804a61fd1a73ce0b03a8c25930af
jerome provensal <552382+jeromegit@users.noreply.github.com>
2022-04-13 17:44:11 -0700

    Update bash_functions_and_aliases.sh
    added gf() and h

--- bash_functions_and_aliases.sh
+++ bash_functions_and_aliases.sh
@@ -1,3 +1,9 @@
 # ls the top 40 most recent files
 lh() { ls -lt $@ | head -40 ; }
 
+# grep a FIX file it assumes that the first arg is the string to search/grep
+#  gf string_to_search [options such as -i or -v] file(s)
+gf() { grep  $* | tr "\001" \| | grep "$1"; };
+
+# history
+alias h=history
